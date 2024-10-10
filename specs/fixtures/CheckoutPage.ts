5172c487ed05f18341506161641b6dc4c1f2a45f
Alessandro Casazza <alessandro@commercelayer.io>
2024-10-10 12:21:46 +0200

    fix: add support for adyen stored payment methods

--- specs/fixtures/CheckoutPage.ts
+++ specs/fixtures/CheckoutPage.ts
@@ -764,7 +764,7 @@ export class CheckoutPage {
         await newPage.locator("#otp_field__container input").fill("123456")
         // await newPage.getByTestId("kaf-button").click()
 
-        const paymentCategory =  newPage.getByTestId("select-payment-category")
+        const paymentCategory = newPage.getByTestId("select-payment-category")
         if (await paymentCategory.isVisible()) {
           paymentCategory.click()
         }
@@ -828,7 +828,7 @@ export class CheckoutPage {
               .click()
             await this.page.getByTestId("save-payment-button").click()
 
-            const klarnaIframe = this.page //.frameLocator("#klarna-apf-iframe")
+            const klarnaIframe = this.page // .frameLocator("#klarna-apf-iframe")
 
             await klarnaIframe
               .getByTestId("kaf-field")
@@ -858,15 +858,15 @@ export class CheckoutPage {
 
             await this.page.waitForTimeout(4000)
             const pickPlan = klarnaIframe.getByTestId("pick-plan")
-            if (await pickPlan.isVisible()){
+            if (await pickPlan.isVisible()) {
               await pickPlan.click()
             }
             await this.page.waitForTimeout(4000)
-            if (await pickPlan.isVisible()){
+            if (await pickPlan.isVisible()) {
               await pickPlan.click()
             }
             const confirmAndPay = klarnaIframe.getByTestId("confirm-and-pay")
-            if (await confirmAndPay.isVisible()){
+            if (await confirmAndPay.isVisible()) {
               await confirmAndPay.click()
             }
             const button = klarnaIframe.getByRole("button", { name: "Weiter" })
@@ -906,7 +906,7 @@ export class CheckoutPage {
             await this.page.click("[data-testid=save-payment-button]")
             await this.page.waitForTimeout(4000)
 
-            const klarnaIframe = this.page //.frameLocator("#klarna-apf-iframe")
+            const klarnaIframe = this.page // .frameLocator("#klarna-apf-iframe")
 
             await klarnaIframe
               .getByTestId("kaf-field")
@@ -944,7 +944,7 @@ export class CheckoutPage {
               // await this.page.click("#buy-button")
 
               // const i = this.page.locator("#klarna-apf-iframe")
-              const klarnaIframe = this.page //.frameLocator("#klarna-apf-iframe")
+              const klarnaIframe = this.page // .frameLocator("#klarna-apf-iframe")
 
               await this.page
                 .getByTestId("kaf-field")
@@ -991,7 +991,7 @@ export class CheckoutPage {
               await this.page.click("[data-testid=save-payment-button]")
               await this.page.waitForTimeout(5000)
 
-              const klarnaIframe = this.page //.frameLocator("#klarna-apf-iframe")
+              const klarnaIframe = this.page // .frameLocator("#klarna-apf-iframe")
 
               await klarnaIframe
                 .getByTestId("kaf-field")
@@ -1098,7 +1098,7 @@ export class CheckoutPage {
             await this.page.waitForTimeout(3000)
             await cardFrame
               .locator("[data-fieldtype=encryptedCardNumber]")
-              .fill("4917610000000000", { timeout: 5000 })
+              .fill("5454545454545454", { timeout: 5000 })
 
             const expFrame = this.page.frameLocator("iframe >> nth=1")
             await expFrame
@@ -1122,13 +1122,14 @@ export class CheckoutPage {
 
   async enter3DSecure({ text }: { type: "adyen"; text: string }) {
     await this.page.waitForTimeout(3000)
+    await this.page.waitForSelector("iframe[name=threeDSIframe]")
     const secureFrame = this.page.frameLocator("iframe[name=threeDSIframe]")
     await this.page.waitForTimeout(2000)
-    const element = await secureFrame.getByPlaceholder(
-      "enter the word 'password'"
-    )
+    await this.page.mouse.wheel(0, 300)
+    const element = secureFrame.getByPlaceholder("enter the word 'password'")
     await element.click()
     await element.fill(text)
+    await this.page.waitForTimeout(2000)
     await secureFrame.locator("#buttonSubmit").click()
   }
 
